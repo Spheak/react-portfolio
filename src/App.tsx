87acc3a5648fd8ab4236c331a6c2ab2539cbcaa9
Sopheak SY <109304944+Spheak@users.noreply.github.com>
2025-03-04 01:06:42 +0700

    cofig dark mode & light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { ThemeProvider } from "@/components/theme-provider/theme-provider";
+import { ModeToggleButton } from "./components/mode-toggle/ModeToggleButton";
 
-
-const App = () => {
+function App() {
   return (
-    <div>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggleButton   />
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
