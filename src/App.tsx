85599d01d1496ef0fb17f6ca18cfa1e1c8f791e4
Sopheak SY <109304944+Spheak@users.noreply.github.com>
2025-03-04 01:37:20 +0700

    config i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { ThemeProvider } from "@/components/theme-provider/theme-provider";
-import { ModeToggleButton } from "./components/mode-toggle/ModeToggleButton";
+// import { ModeToggleButton } from "./components/mode-toggle/ModeToggleButton";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import { t } from "i18next";
 
 function App() {
+  const {
+    i18n: { changeLanguage, language },
+  } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState(language);
+  const handleChangeLanguage = () => {
+    const newLanguage = currentLanguage === "en" ? "kh" : "en";
+    setCurrentLanguage(newLanguage);
+    changeLanguage(newLanguage);
+  };
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggleButton   />
+      {/* <ModeToggleButton   /> */}
+      <div>{t("hello")}</div>
+      <button type="button" onClick={handleChangeLanguage}>
+        Change Language
+      </button>
     </ThemeProvider>
   );
 }
